d2df9829f8a54844e2dc566c4f494178f0a9a2a7
woleigh <woleigh2000@gmail.com>
2014-09-08 23:23:26 +0100

    Fizz Buzz App
    Input box centralized, text added above input box and comments added to
    js file

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 	$(document).ready(function(){
+		// Make the Enter Key functional
 		$(document).keydown(function(event){
 		var numberAdd =$("input[name=search]").val();
     	if (event.which == 13){
     		event.preventDefault();
+    	// Remove any displayed text when enter key is pressed
     		$('.main p').remove();
-			
+		// Declare Overall Fizz Buzz App Conditions
 			for (var i = 1; i <= numberAdd; i++){
 	
 				if (i % 5=== 0 && i % 3=== 0){
