f4954b7bddd51eff3d1d286ecac0f8dd38225500
woleigh <woleigh2000@gmail.com>
2014-09-09 21:21:05 +0100

    Fizz Buzz App with Input Refactored
    Variable declaration added

--- app.js
+++ app.js
@@ -2,29 +2,31 @@
 		// Make the Enter Key functional
 		$(document).keydown(function(event){
 		var numberAdd =$("input[name=search]").val();
+		var textDelete = $('.main p');
+		var mainAdd = $('.main')
     	if (event.which == 13){
     		event.preventDefault();
     	// Remove any displayed text when enter key is pressed
-    		$('.main p').remove();
+    		textDelete.remove();
 		// Declare Overall Fizz Buzz App Conditions
 			for (var i = 1; i <= numberAdd; i++){
 	
 				if (i % 5=== 0 && i % 3=== 0){
 				//console.log("Fizz Buzz")
-				$('.main').append("<p>Fizz Buzz</p>");
+				mainAdd.append("<p>Fizz Buzz</p>");
 				}
 				else if (i % 5 === 0){
 				//console.log ("buzz");
-				$('.main').append("<p>Buzz</p>");
+				mainAdd.append("<p>Buzz</p>");
 
 				}
 				else if (i % 3 === 0){
 				//console.log ("fizz");
-				$('.main').append("<p>Fizz</p>")
+				mainAdd.append("<p>Fizz</p>")
 				}
 				else {
 				//console.log(i);
-				$('.main').append("<p>" + i +"</p>");
+				mainAdd.append("<p>" + i +"</p>");
 				}
 
 			}
